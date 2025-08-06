4b886fbee620e2dcceaa246c056d2d2469d8a2a4
Robert Roos <rootzrob@gmail.com>
2025-08-06 11:26:43 +0300

    Part1

--- part1/eslint.config.js
+++ part1/eslint.config.js
@@ -1,7 +1,18 @@
-export default [
-  { ignores: ['dist'] },
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -11,24 +22,8 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-
-      'react/prop-types': 0,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
-]
+])
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,46 +1,54 @@
-import { useState } from 'react'
-const Display = (props) => {
+const Header = ({ course }) => {return( <h1>{course}</h1> )}
+
+const Content = ({ parts }) => {return( <div>
+  <Part part={parts[0]}></Part>
+  <Part part={parts[1]}></Part>
+  <Part part={parts[2]}></Part>
+</div>)}
+
+const Part = ({ part }) => {
   return (
-    <div>{props.counter}</div>
+    <p>
+      {part.name} {part.exercises}
+    </p>
   )
 }
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
   return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+    <p>
+      Number of exercises {total}
+    </p>
   )
 }
 
 const App = () => {
-  const [value, setValue] = useState(10)
-  
-
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // print the new value to console
-    setValue(newValue)
-  }
+const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   
   return (
     <div>
-      {value}
-
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
- 
+
 export default App
